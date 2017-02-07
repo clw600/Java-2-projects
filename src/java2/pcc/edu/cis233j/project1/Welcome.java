787391132ffc2785abc884fa502d40aa288a87eb
clw600 <Christopher.Williams20@pcc.edu>
2017-02-06 23:09:56 -0800

    print date

--- src/java2/pcc/edu/cis233j/project1/Welcome.java
+++ src/java2/pcc/edu/cis233j/project1/Welcome.java
@@ -1,5 +1,7 @@
 package java2.pcc.edu.cis233j.project1;
 
+import java.util.Date;
+
 /**
  * Simple Text Printing Class to produce Welcome message
  * for my first IntelliJ project
@@ -7,6 +9,7 @@ package java2.pcc.edu.cis233j.project1;
 public class Welcome {
     public static void main(String[] args)  {
         System.out.println("Welcome to my first Project");
+        System.out.println(new Date());
     }
 
         }
