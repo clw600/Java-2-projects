1888fa3f81de4ba02a057c7b9ca711172a87232e
clw600 <Christopher.Williams20@pcc.edu>
2017-02-06 23:29:41 -0800

    added my name to project comments

--- src/java2/pcc/edu/cis233j/project1/Welcome.java
+++ src/java2/pcc/edu/cis233j/project1/Welcome.java
@@ -5,6 +5,9 @@ import java.util.Date;
 /**
  * Simple Text Printing Class to produce Welcome message
  * for my first IntelliJ project
+ * Creator Christopher Williams -CIS 233j
+ * 02-06-2017
+ *
  */
 public class Welcome {
     public static void main(String[] args)  {
